0e09b5e7bb26ac7cb639acc45013d4bf622540f2
tuk86214 <tuk86214@temple.edu>
2024-09-26 11:30:25 -0400

    Defined Lambda and passed lambda to the adapter

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -18,10 +18,12 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
+        val onItemClick: (Int) -> Unit = { number ->
+            displayTextView.text = "You selected: $number"
+        }
 
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
-
+        recyclerView.adapter = NumberDisplayAdapter(numbers, onItemClick)
 
     }
 
